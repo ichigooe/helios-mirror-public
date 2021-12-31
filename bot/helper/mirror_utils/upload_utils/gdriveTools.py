ed072c212e86782716258812d9158bea4bac481b
lcijien11 <94223482+lcijien11@users.noreply.github.com>
2021-12-31 20:09:03 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -678,7 +678,7 @@ class GoogleDriveHelper:
             elif not response["files"]:
                 continue
             if not Title:
-                msg += f'<h4>Search Result For <i> {fileName} </i> </h4><br> Owner </a></b> <br>'
+                msg += f'<h4>Search Result For <i> {fileName} </i> </h4>'
                 Title = True
             if len(DRIVES_NAMES) > 1 and DRIVES_NAMES[index] is not None:
                 msg += f"╾────────────╼<br><b>{DRIVES_NAMES[index]}</b><br>╾────────────╼<br>"
