14d766242b25499e14468716dbfafdc815176708
lcijien11 <94223482+lcijien11@users.noreply.github.com>
2021-12-31 16:24:25 +0530

    Update wserver.py

--- wserver.py
+++ wserver.py
@@ -208,8 +208,8 @@ input[type="submit"]:hover, input[type="submit"]:focus{
         </a>
       </div>
       <div class="social">
-        <a href="https://www.github.com/anasty17/mirror-leech-telegram-bot"><i class="fab fa-github"></i></a>
-        <a href="https://t.me/anas_tayyar"><i class="fab fa-telegram"></i></a>
+        <a href="https://t.me/jkxmirror"><i class="fab fa-github"></i></a>
+        <a href="https://t.me/jkxmirror"><i class="fab fa-telegram"></i></a>
       </div>
     </header>
     <section>
@@ -684,7 +684,7 @@ async def set_priority(request):
 @routes.get('/')
 async def homepage(request):
 
-    return web.Response(text="<h1>See mirror-leech-telegram-bot <a href='https://www.github.com/anasty17/mirror-leech-telegram-bot'>@GitHub</a> By <a href='https://github.com/anasty17'>Anas</a></h1>", content_type="text/html")
+    return web.Response(text="<h1>See mirror-leech-telegram-bot <a href='https://t.me/jkxmirror'>@GitHub</a> By <a href='https://t.me/jkxmirror'>Anas</a></h1>", content_type="text/html")
 
 async def e404_middleware(app, handler):
 
@@ -693,11 +693,11 @@ async def e404_middleware(app, handler):
         try:
             response = await handler(request)
             if response.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>mirror-leech-telegram-bot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>JKxbots</h3>", content_type="text/html")
             return response
         except web.HTTPException as ex:
             if ex.status == 404:
-                return web.Response(text="<h1>404: Page not found</h2><br><h3>mirror-leech-telegram-bot</h3>", content_type="text/html")
+                return web.Response(text="<h1>404: Page not found</h2><br><h3>JKxbots</h3>", content_type="text/html")
             raise
     return middleware_handler
 
