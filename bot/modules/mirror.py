e04849e42ca71f1e49dfe71c1b97f6aa4ed4bac7
lcijien11 <94223482+lcijien11@users.noreply.github.com>
2021-12-31 19:25:08 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -230,7 +230,7 @@ class MirrorListener(listeners.MirrorListeners):
                 sendMessage(msg, self.bot, self.update)
             else:
                 chat_id = str(self.message.chat.id)[4:]
-                msg += f'\n<b>Leeched By: </b>{uname}\n\n'
+                msg += f'\n<b>Leeched By: </b>{uname}\n Also join Leech Channel\n\n'
                 fmsg = ''
                 for index, item in enumerate(list(files), start=1):
                     msg_id = files[item]
